d5fffd4a7e7531df03df58e551020d7bb7ea18e0
leedd87 <leedav87@gmail.com>
2022-09-02 18:39:42 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Index from "./pages/Index";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import AddNewArticlesPage from "./pages/AddNewArticlesPage";
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
+			<Routes>
+				<Route path="/" element={<Index />} />
+				<Route path="/addarticle" element={<AddNewArticlesPage />} />
+			</Routes>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/pages/AddNewArticlesPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddNewArticlesPage = () => {
+	return <div>AddNewArticlesPage</div>;
+};
+
+export default AddNewArticlesPage;
