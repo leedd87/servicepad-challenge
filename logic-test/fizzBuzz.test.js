84eb4939ef9bdc2c4794490cdc0471de847cf5ec
leedd87 <leedav87@gmail.com>
2022-09-02 01:01:00 -0300

    testing fizzbuzz with jest

--- logic-test/fizzBuzz.test.js
+++ logic-test/fizzBuzz.test.js
@@ -5,5 +5,13 @@ test("returns 1 for 1", () => {
 });
 
 test("return fizz for 3", () => {
-	expect(fizzBuzz(3)).toEqual("fizz");
+	expect(fizzBuzz(3)).toBe("fizz");
+});
+
+test("return buzz for 5", () => {
+	expect(fizzBuzz(5)).toBe("buzz");
+});
+
+test("return fizz buzz for 15", () => {
+	expect(fizzBuzz(15)).toBe("fizz buzz");
 });
